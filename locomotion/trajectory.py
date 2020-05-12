fix for units issue for getCurveData

locomotion/trajectory.py
@@ -121,8 +121,11 @@ def getCurveData( animal_obj , col_names = ['X', 'Y']):
       coords.append(smooth(animal_obj.getRawVals(col), animal_obj.getFrameRate()))
     except KeyError:
       raise Exception("column name {} does not exist in animal dataset".format(col))
-  d1 = getDerivatives(coords, axis = 1)
-  d2 = getDerivatives(d1, axis = 1)
+  coords = np.array(coords) / animal_obj.getPixelDensity() # MM
+  d1 = getDerivatives(coords, axis = 1) # MM per frame
+  d1 = d1 * animal_obj.getFrameRate() # MM per second
+  d2 = getDerivatives(d1, axis = 1) # MM per second per frame
+  d2 = d2 * animal_obj.getFrameRate() # MM per second per second
   V = getVelocity(d1)
   C = getCurvature(d1, d2, V)
 